c3897f25c2bd2548c4a212239cdfbdf743b1c29f
MihailUse <alkaevmih@gmail.com>
2021-11-26 23:24:41 +0300

    add dal class
    Signed-off-by: MihailUse <alkaevmih@gmail.com>

---
+++ API/src/database/DAL.ts
@@ -0,0 +1,32 @@
+import { Op } from "sequelize/types";
+import { ApplicationError } from "../router/ApplicationError";
+import { HTTPStatus } from "../utils";
+import { User } from "./models/User";
+
+
+export class DAL {
+
+    static async tryGetUser(id: string | number, options: any = {}): Promise<User> {
+        let ret;
+        const asNum = Number(id);
+        if (!Number.isNaN(asNum)) {
+            ret = await User.findByPk(id, options);
+        } else if (typeof id === "string") {
+            ret = await User.findOne({
+                ...options,
+                where: {
+                    shortLink: {
+                        [Op.iLike]: `%${id.toLowerCase()}%`
+                    }
+                }
+            });
+        }
+
+        if (!ret) {
+            throw new ApplicationError(HTTPStatus.BAD_REQUEST, `user ${id} not found`);
+        }
+
+        return ret;
+    }
+
+}
