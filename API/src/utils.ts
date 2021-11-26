c55e3fb2eb920fa1f7ed26cb63a42e8a68de6bf1
MihailUse <alkaevmih@gmail.com>
2021-11-26 22:35:31 +0300

    add jwt
    Signed-off-by: MihailUse <alkaevmih@gmail.com>

--- API/src/utils.ts
+++ API/src/utils.ts
@@ -1,3 +1,7 @@
+import { NextFunction, Request, Response } from "express";
+import { ApplicationError } from "./router/ApplicationError";
+import jwt from "jsonwebtoken";
+
 export enum HTTPStatus {
     SUCCESS = 200,
     BAD_REQUEST = 400,
@@ -8,4 +12,74 @@ export enum HTTPStatus {
     INTERNAL = 500,
     NOT_IMPLEMENTED = 501,
     SERVICE_UNAVAILABLE = 503,
+}
+
+export interface ValidateJWTResponse {
+    user: User;
+    iat: number;
+    exp: number;
+}
+
+async function validateJwt(options: { token: string }) {
+    try {
+        const { token } = options;
+
+        if (!token) {
+            throw new ApplicationError(HTTPStatus.BAD_REQUEST, "All parameters are required");
+        }
+
+        if (!options) {
+            throw new ApplicationError(HTTPStatus.BAD_REQUEST, "Parameters is required");
+        }
+
+        const verify = await jwt.verify(token, process.env.TOKEN_SECRET);
+
+        if (!verify) {
+            throw new ApplicationError(HTTPStatus.UNAUTHORIZED, "Unauthorized");
+        }
+
+        const user = await User.findByPk(verify.data.id, {
+            attributes: {
+                exclude: ["password"],
+            }
+        });
+
+        if (!user) {
+            throw new ApplicationError(HTTPStatus.NOT_FOUND, "User does not exist");
+        }
+
+        return {
+            status: 200,
+            message: "OK",
+            user,
+            iat: verify.iat,
+            exp: verify.exp,
+        };
+    } catch (e) {
+        throw e;
+    }
+}
+
+export default async function validateJWT(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const token = req?.headers?.authorization?.split(" ")[1];
+        
+        if (!token) {
+            throw new ApplicationError(HTTPStatus.NOT_FOUND ,"Token not defined");
+        }
+
+        const response: ValidateJWTResponse = await validateJwt({
+            token: token,
+        });
+
+        req.body.token = response;
+        next();
+    } catch (err) {
+        console.log(err);
+        res.status(err?.status || HTTPStatus.INTERNAL).json(err);
+    }
 }
